08b8d4e5030263bf511a3b0b5661fbd29c7854cb
caopu <17600201849@163.com>
2019-03-14 11:18:23 +0800

    第一次提交

---
+++ src/main/java/com/abc/springboot/controller/Helloworld.java
@@ -0,0 +1,12 @@
+package com.abc.springboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Helloworld {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "你好，China!";
+    }
+}
